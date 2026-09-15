b7a70b1bce18cb2dbc3d39bc8e9a827042b1daa0
Bakdaulet <b_shamil@kbtu.kz>
2026-09-15 23:41:32 +0500

    Add Python data types examples

---
+++ Practice1/python-basics/data_types.py
@@ -0,0 +1,11 @@
+name = "Bakdaulet"
+age = 19
+height = 188.5
+student = True
+subjects = ["Python", "Math", "English"]
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(student))
+print(type(subjects))
