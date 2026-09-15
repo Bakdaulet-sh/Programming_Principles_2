1cf989f20d84401f9ad888d0ac890a901c1cd6db
Bakdaulet <b_shamil@kbtu.kz>
2026-09-15 23:37:59 +0500

    Add Python syntax examples

---
+++ Practice1/python-basics/syntax.py
@@ -0,0 +1,14 @@
+print("Hello, World!")
+
+x = 10
+y = 5
+print(x + y)
+
+name = "Bakdaulet"
+print("Hello", name)
+
+age = 19
+print("I am", age, "years old")
+
+if age >= 18:
+    print("I am an adult")
